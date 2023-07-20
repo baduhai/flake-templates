77d5e6651dc7fb24cc839535d4dfd3a2e789fb5e
baduhai <baduhai@pm.me>
2023-07-20 12:07:37 -0300

    Created python environment template

---
+++ python/flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Flake template for a python project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in with pkgs; {
+        devShells.default = mkShell {
+          buildInputs = [ python310 ];
+          packages = [ ];
+        };
+      });
+}
