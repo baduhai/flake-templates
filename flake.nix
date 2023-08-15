a634e932f6ee9cfd3dd26b393c91b3372e67ae1b
William <baduhai@proton.me>
2023-08-15 10:33:31 -0300

    Added C template

--- flake.nix
+++ flake.nix
@@ -3,17 +3,21 @@
 
   outputs = { self }: {
     templates = {
+      c = {
+        path = ./c;
+        description = "Template for a C project";
+      };
       package = {
         path = ./package;
         description = "Template for a nix package";
       };
       python = {
         path = ./python;
-        description = "Template to python project";
+        description = "Template for a python project";
       };
       rust = {
         path = ./rust;
-        description = "Rust template, using oxalica/rust-overlay";
+        description = "Template for a rust project, using oxalica/rust-overlay";
       };
       shell = {
         path = ./shell;
